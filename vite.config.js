a607188b76bcc2e76d71583d47c9073e90b6c5f0
Benoit Blanchon <git@benoitblanchon.fr>
2022-07-22 10:25:23 +0200

    Make internal links absolute in development

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,29 @@
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Markdown from 'vite-plugin-md'
+import { defineConfig } from "vite"
+import Vue from "@vitejs/plugin-vue"
+import Markdown, { link } from "vite-plugin-md"
+
+function makeInternalLinkAbsolute(lnk) {
+  lnk.href = new URL(lnk.href, "https://arduinojson.org").href
+  return lnk
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     Vue({
-      include: [/\.vue$/, /\.md$/],
+      include: [/\.vue$/, /\.md$/]
     }),
-    Markdown(),
-  ],
-})
+    Markdown({
+      builders: [
+        link({
+          useRouterLinks: false,
+          internalLinkClass: undefined,
+          internalTarget: "_blank",
+          externalLinkClass: undefined,
+          postProcessing:
+            mode == "development" ? makeInternalLinkAbsolute : undefined
+        })
+      ]
+    })
+  ]
+}))
