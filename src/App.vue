cf186e69d4e3c38ef4aacc6672c332390d061e3f
Benoit Blanchon <git@benoitblanchon.fr>
2022-07-22 14:47:53 +0200

    Extract AssistanceModal component

--- src/App.vue
+++ src/App.vue
@@ -75,150 +75,14 @@
       >
         {{ reportCopied ? "✓ Report copied" : "Copy troubleshooter's report" }}
       </button>
-      <div class="modal fade" id="assistance-modal">
-        <div class="modal-dialog modal-dialog-scrollable modal-lg">
-          <form
-            class="modal-content"
-            ref="issueForm"
-            @submit.prevent="createIssue"
-          >
-            <div class="modal-header bg-primary text-white">
-              <h5 class="modal-title">Contact support</h5>
-              <button
-                type="button"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <div class="alert alert-info">
-                This form simplifies the creation of an issue on ArduinoJson's
-                GitHub project page. It ensures you provide all the required
-                information and prefills the issue's text. After that, you just
-                need to submit the issue.<br />
-                <b>A GitHub account is required.</b>
-              </div>
-              <div class="form-group">
-                <label for="title-input">Title</label>
-                <input
-                  type="text"
-                  name="title"
-                  class="form-control"
-                  id="title-input"
-                  aria-describedby="title-help"
-                  required
-                />
-                <small id="title-help" class="form-text text-muted"
-                  >Summarize your issue in one sentence.</small
-                >
-              </div>
-              <div class="form-group">
-                <label for="description-input">Description</label>
-                <textarea
-                  name="description"
-                  rows="3"
-                  class="form-control"
-                  id="description-input"
-                  aria-describedby="description-help"
-                  required
-                  placeholder="When I do ..., I expect ..., but instead I get ..."
-                ></textarea>
-                <small id="description-help" class="form-text text-muted"
-                  >Describe your issue with a few sentences.</small
-                >
-              </div>
-              <div class="form-group">
-                <label for="mcu-input">Microcontroller</label>
-                <input
-                  type="text"
-                  name="mcu"
-                  class="form-control"
-                  id="mcu-input"
-                  aria-describedby="mcu-help"
-                  placeholder="ESP8266"
-                  required
-                />
-                <small id="mcu-help" class="form-text text-muted"
-                  >Which processor or with board do you use?</small
-                >
-              </div>
-              <div class="form-group">
-                <label for="framework-input">Arduino Core / Framework</label>
-                <input
-                  type="text"
-                  name="framework"
-                  class="form-control"
-                  id="framework-input"
-                  aria-describedby="framework-help"
-                  placeholder="ESP8266 core for Arduino v3.0.2"
-                />
-                <small id="framework-help" class="form-text text-muted"
-                  >Please include version number.</small
-                >
-              </div>
-              <div class="form-group">
-                <label for="ide-input">IDE</label>
-                <input
-                  type="text"
-                  name="ide"
-                  class="form-control"
-                  id="ide-input"
-                  aria-describedby="ide-help"
-                  placeholder="Arduino IDE 1.8.16"
-                />
-                <small id="ide-help" class="form-text text-muted"
-                  >Please include version number.</small
-                >
-              </div>
-              <div class="form-group">
-                <label for="repro-input">Reproduction code</label>
-                <textarea
-                  name="repro"
-                  rows="10"
-                  class="form-control text-monospace"
-                  style="font-size: 80%"
-                  id="repro-input"
-                  aria-describedby="repro-help"
-                  placeholder="DynamicJsonDocuemnt doc(1024);\n..."
-                ></textarea>
-                <small id="repro-help" class="form-text text-muted"
-                  >Write a few lines of code that demonstrate the issue.</small
-                >
-              </div>
-              <div class="form-group">
-                <label for="remarks-input">Remarks</label>
-                <textarea
-                  name="remarks"
-                  rows="4"
-                  class="form-control"
-                  id="remarks-input"
-                  aria-describedby="remarks-help"
-                ></textarea>
-                <small id="remarks-help" class="form-text text-muted"
-                  >Anything else you need to tell us?</small
-                >
-              </div>
-            </div>
-            <div class="modal-footer">
-              <div class="text-right">
-                <button type="submit" class="btn btn-primary">Submit</button>
-                <small class="form-text text-muted">
-                  This button redirects you to GitHub.
-                </small>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+      <AssistanceModal id="assistance-modal" :report="report" />
     </div>
   </div>
 </template>
 
 <script>
 import pages from "./pages"
+import AssistanceModal from "./components/AssistanceModal.vue"
 
 const sleep = (m) => new Promise((r) => setTimeout(r, m))
 
@@ -240,6 +104,7 @@ function makeStep(pageKey, hash) {
 }
 
 export default {
+  components: { AssistanceModal },
   setup() {
     return {
       intro: pages["intro"].component
@@ -259,9 +124,6 @@ export default {
   computed: {
     currentStep() {
       return this.steps[this.steps.length - 1]
-    },
-    currentStepIsMissing() {
-      return !this.currentStep
     },
     needsAssistance() {
       return !this.currentStep || this.currentStep.needs_assistance
@@ -315,46 +177,6 @@ export default {
       this.reportCopied = true
       await sleep(2000)
       this.reportCopied = false
-    },
-    createIssue() {
-      const formData = new FormData(this.$refs.issueForm)
-
-      const title = formData.get("title")
-      let body = ""
-
-      const description = formData.get("description")
-      if (description) body += `**Description**\n${description}\n\n`
-
-      body += `**Troubleshooter's report**\n${this.report}\n\n`
-
-      const mcu = formData.get("mcu")
-      const framework = formData.get("framework")
-      const ide = formData.get("ide")
-      if (mcu || framework || ide) {
-        body += "**Environment**\n"
-        if (mcu) body += "* Microcontroller: " + mcu + "\n"
-        if (framework) body += "* Core/Framework: " + framework + "\n"
-        if (ide) body += "* IDE: " + ide + "\n"
-        body += "\n"
-      }
-
-      const repro = formData.get("repro")
-      if (repro) {
-        body += "**Reproduction code**\n```c++\n"
-        body += repro
-        body += "\n```\n\n"
-      }
-
-      const remarks = formData.get("remarks")
-      if (remarks) body += `**Remarks**\n${remarks}\n\n`
-
-      const queryString = new URLSearchParams({ title, body }).toString()
-      const url =
-        "https://github.com/bblanchon/ArduinoJson/issues/new?" + queryString
-      console.log("URL", url)
-      window.open(url, "_blank")
-
-      $("#assistance-modal").modal("hide")
     }
   }
 }
